b50bee063dddc8a63342158095707fc837e320b8
tjqiu <qiutianjie@outlook.com>
2021-08-15 23:27:18 +0800

    add a few components

---
+++ frontend/todo-app/src/components/learning-example/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+//Class Component
+export default class FirstComponent extends Component {
+    render() {
+      return (
+        <div className="FirstComponent">
+          FirstComponent
+        </div>
+      );
+    }
+  }
---
+++ frontend/todo-app/src/components/learning-example/SecondComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SecondComponent () {
+  return (
+    <div className="SecondComponent">
+      SecondComponent
+    </div>
+  );
+}
+
+export default SecondComponent;
