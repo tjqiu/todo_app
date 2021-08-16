acbac720a2be5f77c69ae99d2f8d7ab13462d80c
tjqiu <qiutianjie@outlook.com>
2021-08-16 10:29:11 +0800

    Counter partially done

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FirstComponent from './components/learning-example/FirstComponent'
 import SecondComponent from './components/learning-example/SecondComponent'
+import Counter from './components/counter/Counter';
 import logo from './logo.svg';
 import './App.css';
  
@@ -8,6 +9,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Counter></Counter>
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearningComponents">
         My Hello World
         <FirstComponent />
         <SecondComponent></SecondComponent>
