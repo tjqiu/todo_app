ebdebc79ed86b38e32e05aa8d538ea48a582c555
tjqiu <qiutianjie@outlook.com>
2021-08-18 09:01:16 +0800

    counter

--- frontend/todo-app/src/components/counter/Counter.jsx
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Counter.css'
 
-function Counter() {
+class Counter extends Component {
+  //Define the initial state
+  constructor() {
+    super();
+    this.state = {
+      counter : 0
+    }
+    this.increment = this.increment.bind(this);
+  }
+
+  render() {
     return (
-        <div className="counter">
-          <button>+1</button>
-          <span className='count'>0</span>
-        </div>
+      <div className="counter">
+        <button onClick={this.increment}>+1</button>
+        <span className='count'>{this.state.counter}</span>
+      </div>
     )
+  }
+
+  increment() { //update the state
+    //console.log('increment');
+    //this.state.counter++;
+    this.setState({
+      counter : this.state.counter + 1
+    })
+  }
 }
+
 
 export default Counter;
