252c742545ae680089e6d25bf1ae4e21279198d8
tjqiu <qiutianjie@outlook.com>
2021-08-19 18:18:55 +0800

    counter done

--- frontend/todo-app/src/components/counter/Counter.jsx
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -56,40 +56,40 @@ class CounterButton extends Component {
     this.state = {
       counter : 0
     }
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
+    // this.increment = this.increment.bind(this);
+    // this.decrement = this.decrement.bind(this);
   }
 
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+{this.props.by}</button>
-        <button onClick={this.decrement}>-{this.props.by}</button>
+        <button onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+        <button onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
         {/*<span className='count'>{this.state.counter}</span>*/}
       </div>
     )
   }
 
-  increment() { //update the state
-    //console.log('increment');
-    //this.state.counter++;
-    this.setState(
-      (prevState) => {
-        return  {counter : prevState.counter + this.props.by}
-      }
-    )
-    this.props.incrementMethod(this.props.by)
-  }
+  // increment() { //update the state
+  //   //console.log('increment');
+  //   //this.state.counter++;
+  //   this.setState(
+  //     (prevState) => {
+  //       return  {counter : prevState.counter + this.props.by}
+  //     }
+  //   )
+  //   this.props.incrementMethod(this.props.by)
+  // }
 
 
-  decrement() { 
-    this.setState(
-      (prevState) => {
-        return  {counter : prevState.counter - this.props.by}
-      }
-    )
-    this.props.decrementMethod(this.props.by)
-  }
+  // decrement() { 
+  //   this.setState(
+  //     (prevState) => {
+  //       return  {counter : prevState.counter - this.props.by}
+  //     }
+  //   )
+  //   this.props.decrementMethod(this.props.by)
+  // }
 }
 
 CounterButton.defaultProps = {
