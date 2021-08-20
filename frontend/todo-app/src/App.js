c12c53dc29d52697fd08ad0a2c2c1a4c65f9f57f
tjqiu <qiutianjie@outlook.com>
2021-08-20 10:46:21 +0800

    username and password done

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import FirstComponent from './components/learning-example/FirstComponent'
 import SecondComponent from './components/learning-example/SecondComponent'
 import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/TodoApp';
 import './App.css';
  
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter></Counter>
+        {/*<Counter></Counter>*/}
+        <TodoApp></TodoApp>
       </div>
     );
   }
---
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+
+
+class TodoApp extends Component {
+    render() {
+        return (
+            <div className='TodoApp'>
+                <LoginComponent />
+            </div>
+        )
+    }
+}
+
+
+class LoginComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state= {
+            username: 'test',
+            password: ''
+        }
+
+        // this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
+    }
+
+    handleChange(event) {
+        console.log(this.state)
+        this.setState(
+            {
+                [event.target.name]: event.target.value
+            }
+        )
+    }
+
+    // handleUsernameChange(event) {
+    //     this.setState(
+    //         {
+    //             username: event.target.value
+    //         }
+    //     )
+    // }
+
+    // handlePasswordChange(event) {
+    //     this.setState(
+    //         {
+    //             password: event.target.value
+    //         }
+    //     )
+    // }
+
+    loginClicked() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+                <button onClick={this.loginClicked}>Login</button>
+            </div>
+        )
+    }
+}
+
+
+export default TodoApp;
