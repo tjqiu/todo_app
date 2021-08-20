ace09598b37c3dbf16e7274d448d074d8b094297
tjqiu <qiutianjie@outlook.com>
2021-08-20 17:27:30 +0800

    add login success and fail info

--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -17,7 +17,9 @@ class LoginComponent extends Component {
         super(props)
         this.state= {
             username: 'test_username',
-            password: ''
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
         }
 
         // this.handleUsernameChange = this.handleUsernameChange.bind(this)
@@ -27,7 +29,7 @@ class LoginComponent extends Component {
     }
 
     handleChange(event) {
-        console.log(this.state)
+        //console.log(this.state)
         this.setState(
             {
                 [event.target.name]: event.target.value
@@ -52,21 +54,43 @@ class LoginComponent extends Component {
     // }
 
     loginClicked() {
-        if (this.state.username==='test_username' && this.state.password==='test_password')
+        if (this.state.username==='test_username' && this.state.password==='test_password') {
             console.log('Successful')
-        else
+            this.setState({showSuccessMessage: true})
+            this.setState({hasLoginFailed: false})
+        }
+        else {
             console.log('Failed')
+            this.setState({showSuccessMessage: false})
+            this.setState({hasLoginFailed: true})
+        }
     }
 
     render() {
         return (
             <div>
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
+                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
                 User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
                 Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
                 <button onClick={this.loginClicked}>Login</button>
             </div>
         )
     }
+}
+
+function ShowInvalidCredentials(props) {
+    if (props.hasLoginFailed) {
+        return <div>Invalid Credentials</div>
+    }
+    return null
+}
+
+function ShowLoginSuccessMessage(props) {
+    if (props.showSuccessMessage) {
+        return <div>Login Successful</div>
+    }
+    return null
 }
 
 
