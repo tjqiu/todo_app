9fc2857ce0c506ab05a5d71b8b1a4c539b11ebde
tjqiu <qiutianjie@outlook.com>
2021-08-22 09:54:35 +0800

    change header

--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -35,9 +35,19 @@ function ErrorComponent() {
 class HeaderComponent extends Component {
     render() {
         return (
-            <div>
-                Header<hr/>
-            </div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="http://www.google.com" className="navbar-brand">tjqiu</a></div>
+                    <ul className="navbar-nav">
+                        <li><Link className="nav-link" to="/welcome/test_username">Home</Link></li>
+                        <li><Link className="nav-link" to="/todos">Todos</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li><Link className="nav-link" to="/login">Login</Link></li>
+                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
